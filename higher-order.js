7a7cd411f945bd0cee3f074e8f536b9518ac46ed
tjlemperle <tjlemperle@gmail.com>
2020-02-16 22:28:34 -0700

    finished higher order

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,7 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(num => num%2 === 0 ? num: null)
 
 
 
@@ -44,7 +44,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices  = prices.map( price => {
+  price * 1.07;
+});
 
 
 
